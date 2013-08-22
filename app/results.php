2fa250978b9c6f367acb47cf0e2772d3677aab47
Cody Byrnes <cody.byrnes@gmail.com>
2013-08-22 10:14:22 -0700

    Posting and getting

---
+++ app/results.php
@@ -0,0 +1,102 @@
+<DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="../css/styles.css" rel="stylesheet" media="screen">
+</head>
+<body>
+<!--NAVIGATION-->
+<nav>
+    <div class="navbar-inverse">
+        <div class="container">
+
+                <a class="navbar-brand">BooshApp</a>
+
+        </div>
+    </div>
+</nav>
+<!--END NAVIGATION-->
+<div class="jumbotron">
+  <div class="container">
+  <h1>Search This SHIT</h1>
+  <p>You know you want to</p>
+  <div class="input-group">
+        <form action="./results.php" method="post">
+            <input type="text" name="search" class="form-control" placeholder="Search Anything">
+            <input style="width:100%;" id="search_input" class="btn btn-default" type="submit" value="submit"> 
+        </form>
+    </div>
+  </div>
+</div>
+<section >
+    <div class="container">
+    	<div class="row">
+    		<div class="col-lg-12 col-sm-12">
+    		<div class="well">
+    			
+
+<?php 
+
+	// echo ($_POST[band_name]); 
+
+	$db = new mysqli('localhost', 'root', 'root', "boosh");
+
+	if( $db->connect_errno > 0){
+		die('Unable to connect ['.$db->connect_error."]");
+	} else {
+	}
+
+
+$sqlGetBands = <<<SQL
+	SELECT * FROM bands
+SQL;
+
+$sqlGetVenues = <<<SQL
+	SELECT * FROM venues
+SQL;
+
+
+
+if(!$result = $db->query($sqlGetBands)){
+	die('<li>Error Posting to database ['.$db->error."]</li></ul>");
+} else {
+	echo "<h2>BANDS</h2>";
+	echo "<table class='table table-striped table-bordered'>";
+	echo "<tr> <th>ID</th> <th> Name </th> </tr>";
+	while($row = $result->fetch_assoc() ){
+		echo "<tr><td>".$row['band_id']."</td><td>".$row['band_name']."</td></tr>";	
+	}
+	echo "</table>";
+}  
+
+if(!$result = $db->query($sqlGetVenues)){
+	die('<li>Error Posting to database ['.$db->error."]</li></ul>");
+} else {
+
+	echo "<h2>VENUES</h2>";
+	echo "<table  class='table table-striped table-bordered'>";
+	echo "<tr> <th>ID</th> <th> Name </th> </tr>";
+
+	while($row = $result->fetch_assoc() ){
+		echo "<tr><td>".$row['venue_id']."</td><td>".$row['venue_name']."</td></tr>";	
+	}
+	echo "</table>";
+}  
+
+ ?>
+
+    		</div>
+    		
+</div>
+    	</div>
+    </div>
+    </section>
+
+<!-- JavaScript plugins (requires jQuery) -->
+<script src="http://code.jquery.com/jquery.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="../js/bootstrap.min.js"></script>
+</body>
+</html>
--- app/venue.php
+++ app/venue.php
@@ -1,30 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Mailing List</title>
-	<style>
-		table{
-			width:100%;
-		} 
-		td{
-			padding: 5px;
-		}
-	</style>
-</head>
-<body>
-<h1>Add New User</h1>
-<form action="." method="post">
-	<input type="text" name="first_name" placeholder="First">
-	<input type="text" name="last_name" placeholder="Last">
-	<input type="text" name="email" placeholder="Email">
-	<input type="submit" value="Submit">
-</form>
 
-<h1>Database Status</h1>
 
 <?php 
 
-	$db = new mysqli('localhost', 'root', 'root', "mailingList");
+	echo ($_POST[band_name]); 
+
+	$db = new mysqli('localhost', 'root', 'root', "boosh");
 
 	if( $db->connect_errno > 0){
 		die('Unable to connect ['.$db->connect_error."]");
@@ -33,54 +13,38 @@
 	}
 
 $sql = <<<SQL
-	INSERT INTO  users (
-	first_name ,
-	last_name ,
-	email
+	INSERT INTO  venues (
+	venue_name 
 	)
 	VALUES (
-	 '$_POST[first_name]',  '$_POST[last_name]',  '$_POST[email]'
+	 '$_POST[venue_name]'
 	);
 SQL;
 
 $sqlGet = <<<SQL
-	SELECT * FROM users
+	SELECT * FROM venues
 SQL;
 
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   	if(!$result = $db->query($sql)){
 		die('<li>Error Posting to database ['.$db->error."]</li></ul>");
 	} else {
 		echo("<li>Post Successful</li></ul>");
 	}  
-}elseif($_SERVER['REQUEST_METHOD'] === 'GET'){
-	echo "</ul>";
-}
+// }elseif($_SERVER['REQUEST_METHOD'] === 'GET'){
+// 	echo "</ul>";
+// }
 
  ?>
 
-
-<h1>All Users</h1>
-
-<table border="1">
-	<tr>
-		<th>First </th>
-		<th>Last</th>
-		<th>Email</th>
-	</tr>
 	<?php 
 
-	if(!$result = $db->query($sqlGet)){
-		die('<li>Error Posting to database ['.$db->error."]</li></ul>");
-	} else {
-		while($row = $result->fetch_assoc() ){
-			echo "<tr><td>".$row['first_name']."</td><td>".$row['last_name']."</td><td><a href='mailto:".$row['email']."''>".$row['email']."</a></td>";	
-		}
-	}  
+	// if(!$result = $db->query($sqlGet)){
+	// 	die('<li>Error Posting to database ['.$db->error."]</li></ul>");
+	// } else {
+	// 	while($row = $result->fetch_assoc() ){
+	// 		echo "<tr><td>".$row['first_name']."</td><td>".$row['last_name']."</td><td><a href='mailto:".$row['email']."''>".$row['email']."</a></td>";	
+	// 	}
+	// }  
 	 ?>
-
-</table>
-
-</body>
-</html>
